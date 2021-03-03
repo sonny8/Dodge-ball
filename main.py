40b80d5ae5418f712cdc2f5243b60ea4e3172286
EmersonLiepe <63976937+EmersonLiepe@users.noreply.github.com>
2021-03-03 11:00:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pygame
+import sys
+pygame.init()
+wind=pygame.display.set_mode((750,650))
+pygame.display.set_caption("Dodge Ball")
+
+while True:
+         for eve in pygame.event.get():
+              if eve.type==pygame.QUIT:
+                   pygame.quit()
+                   sys.exit()
+
+player = Actor("diamond_s.png")
+
+def draw():
+    screen.fill("black")
+    player.draw()
+
 
