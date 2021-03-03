1af38301d30199835b07639833dca3f3ad35c1e6
EmersonLiepe <63976937+EmersonLiepe@users.noreply.github.com>
2021-03-03 11:15:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import pygame
-import sys
-pygame.init()
-wind=pygame.display.set_mode((750,650))
-pygame.display.set_caption("Dodge Ball")
-
-while True:
-         for eve in pygame.event.get():
-              if eve.type==pygame.QUIT:
-                   pygame.quit()
-                   sys.exit()
-
-player = Actor("diamond_s.png")
+import pgzero
+from pgzero.builtins import Actor, animate, keyboard
+#player = Actor("diamond_s.png")
 
 def draw():
     screen.fill("black")
