5b6acbbcf47fff608ebf2f83628b0532fda6c467
EmersonLiepe <51467994+sonny8@users.noreply.github.com>
2021-03-04 09:14:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pgzero
 from pgzero.builtins import Actor, animate, keyboard
-#player = Actor("diamond_s.png")
+player = Actor("diamond_s.png")
 
 def draw():
     screen.fill("black")
     player.draw()
 
 
+C
