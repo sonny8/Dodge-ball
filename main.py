a95d12e7b23c00616ee3fb1978f091b1c4debc45
EmersonLiepe <51467994+sonny8@users.noreply.github.com>
2021-03-05 08:26:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pgzero
 from pgzero.builtins import Actor, animate, keyboard
-path = "C:/Users/User/Desktop/Github/Programs/Dodge-ball/images/diamond_s.png"
-player = Actor(path)
+playerimage = "diamond_s.png"
+ballimage = "dodge-ball-brown"
+player = Actor(playerimage)
+balls = Actor(ballimage)
 
 def draw():
     screen.fill("black")
     player.draw()
+    balls.draw()
 
 
 
