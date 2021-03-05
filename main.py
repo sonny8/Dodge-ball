a0beb8b399675f819e31ca0d2a7311f5fd3036a8
EmersonLiepe <51467994+sonny8@users.noreply.github.com>
2021-03-05 09:14:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import pgzero
 from pgzero.builtins import Actor, animate, keyboard
+
 playerimage = "diamond_s.png"
 ballimage = "dodge-ball-brown"
 player = Actor(playerimage)
 balls = Actor(ballimage)
+WIDTH = 500
+HEIGHT = WIDTH
 
 def draw():
     screen.fill("black")
     player.draw()
     balls.draw()
 
+
+def update():
+
+    if keyboard.right:
+        player.x += 5
+    if keyboard.left:
+        player.x += -5
+
+    if keyboard.up:
+        player.y += -5
+
+
+    if keyboard.down:
+        player.y += 5
+
+    if player.left > WIDTH:
+        player.right = 0
 
 
