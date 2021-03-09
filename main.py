b84d9c3066dc5329b263d83fea29d42b567efe62
EmersonLiepe <51467994+sonny8@users.noreply.github.com>
2021-03-09 09:18:42 -0700

    Created infinite world.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pgzero
 from pgzero.builtins import Actor, animate, keyboard
 
-playerimage = "diamond_s.png"
+playerimage = "alien"
 ballimage = "dodge-ball-brown"
 player = Actor(playerimage)
 balls = Actor(ballimage)
 WIDTH = 500
 HEIGHT = WIDTH
+
 
 def draw():
     screen.fill("black")
@@ -24,11 +25,16 @@ def update():
     if keyboard.up:
         player.y += -5
 
-
     if keyboard.down:
         player.y += 5
 
     if player.left > WIDTH:
         player.right = 0
+    if player.right < 0:
+        player.left = WIDTH
 
+    if player.bottom < 0:
+        player.top = HEIGHT
 
+    if player.top > HEIGHT:
+        player.bottom = 0
